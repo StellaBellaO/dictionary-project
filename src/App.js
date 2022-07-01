995133bfaa6f40d2ad6f7684d0b8795c506c53e1
StellaBellaO <101139955+StellaBellaO@users.noreply.github.com>
2022-07-01 16:20:19 +0100

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from "./logo.jpeg";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
        <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer text-center"> <small>Coded by Stella-Maris Ojideh </small> </footer>
+      </div>
     </div>
+    
   );
 }
 
