733789caa1ac37941dcb9844218ab3113cd01320
StellaBellaO <101139955+StellaBellaO@users.noreply.github.com>
2022-07-01 16:46:53 +0100

    added axios and tried making calls to api. added heading

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <div className="container">
       <header className="App-header">
        <img src={logo} className="App-logo img-fluid" alt="logo" />
+       <h1 className="text-center"> SMEO Dictionary </h1>
       </header>
       <main>
         <Dictionary />
