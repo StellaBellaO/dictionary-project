dc6fc2cb84042cc9298ea36866d454ab927cf2a1
StellaBellaO <101139955+StellaBellaO@users.noreply.github.com>
2022-07-04 16:14:46 +0100

    amended footer

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <main>
         <Dictionary defaultKeyword="Ocean" />
       </main>
-      <footer className="App-footer text-center"> <small>Coded by Stella-Maris Ojideh </small> </footer>
+      <footer className="App-footer text-center"> <small>🗄️ This Project was coded by < a href="https://gleeful-bubblegum-ab91e6.netlify.app/index.html" target="_blank" rel="noopener noreferrer"> Stella-Maris Ojideh </a> {" "} and is {" "} <a href="https://github.com/StellaBellaO/dictionary-project" target="_blank" rel="noopener noreferrer" class="github">open-sourced on GitHub </a> {" "} and <a href="https://app.netlify.com/sites/spiffy-manatee-3e127b/overview" target="_blank" rel="noopener noreferrer"> hosted on Netlify 🗄️ </a> </small> </footer>
       </div>
     </div>
     
